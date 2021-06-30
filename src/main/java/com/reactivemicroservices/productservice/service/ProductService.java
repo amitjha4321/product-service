ff89c71dbd6be022c25d564a0770826a5b31c37e
Amit Jha <amitjha4321@gmail.com>
2021-07-01 01:50:29 +0545

    updateProduct added in the repository

--- src/main/java/com/reactivemicroservices/productservice/service/ProductService.java
+++ src/main/java/com/reactivemicroservices/productservice/service/ProductService.java
@@ -34,4 +34,19 @@ public class ProductService {
         //when we insert this middle line will give us backthe mono of updated entity with id so we have used flat map to extract that
         //and then we again converted it back to dto and returned it
     }
+    public Mono<ProductDto> updateProduct(String id, Mono<ProductDto> productDtoMono){
+        return this.repository.findById(id)
+                .flatMap(p->productDtoMono
+                .map(EntityDtoUtil::toEntity)
+                .doOnNext(e->e.setId(id)))
+                .flatMap(this.repository::save)
+                .map(EntityDtoUtil::toDto);
+        //we are having an id , we are checking in the repository with the id is present, if it is present this will emit an entity
+        //object if present when we you emit that dto I am going to convert it to entity once I get the entity I am going to set the
+        //id because Id field could be blank , could be null once the id is set since we have mono within the mono we used flatmap
+        //then we have entity object , we are using repository to save that information this will again return mono of the updated
+        //product information so we are again using flatmap now in the second last line I have the entity and hence changing it to dto
+        //and return this
+
+    }
 }
